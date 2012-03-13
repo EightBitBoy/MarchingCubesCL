2611f26fd3bd43cc1ce1db1294fd365d4ece0bc6
Stefan Haertel <haertel.dev@gmail.com>
2012-03-13 07:21:57 +0100

    some optimization

--- MarchingCubesKernel.hpp
+++ MarchingCubesKernel.hpp
@@ -46,18 +46,30 @@ std::string getKernelSource()
 			if (values[(i * 8) + 7] < isoValue) index |= 128;
 			indices[i] = index;
 
-			triPoints[(i * 12) +  0] = interpolate(isoValue, pointsVec[(i * 8) + 0], pointsVec[(i * 8) + 1], values[(i * 8) + 0], values[(i * 8) + 1]);
-			triPoints[(i * 12) +  1] = interpolate(isoValue, pointsVec[(i * 8) + 1], pointsVec[(i * 8) + 2], values[(i * 8) + 1], values[(i * 8) + 2]);
-			triPoints[(i * 12) +  2] = interpolate(isoValue, pointsVec[(i * 8) + 2], pointsVec[(i * 8) + 3], values[(i * 8) + 2], values[(i * 8) + 3]);
-			triPoints[(i * 12) +  3] = interpolate(isoValue, pointsVec[(i * 8) + 3], pointsVec[(i * 8) + 0], values[(i * 8) + 3], values[(i * 8) + 0]);
-			triPoints[(i * 12) +  4] = interpolate(isoValue, pointsVec[(i * 8) + 4], pointsVec[(i * 8) + 5], values[(i * 8) + 4], values[(i * 8) + 5]);
-			triPoints[(i * 12) +  5] = interpolate(isoValue, pointsVec[(i * 8) + 5], pointsVec[(i * 8) + 6], values[(i * 8) + 5], values[(i * 8) + 6]);
-			triPoints[(i * 12) +  6] = interpolate(isoValue, pointsVec[(i * 8) + 6], pointsVec[(i * 8) + 7], values[(i * 8) + 6], values[(i * 8) + 7]);
-			triPoints[(i * 12) +  7] = interpolate(isoValue, pointsVec[(i * 8) + 7], pointsVec[(i * 8) + 4], values[(i * 8) + 7], values[(i * 8) + 4]);
-			triPoints[(i * 12) +  8] = interpolate(isoValue, pointsVec[(i * 8) + 0], pointsVec[(i * 8) + 4], values[(i * 8) + 0], values[(i * 8) + 4]);
-			triPoints[(i * 12) +  9] = interpolate(isoValue, pointsVec[(i * 8) + 1], pointsVec[(i * 8) + 5], values[(i * 8) + 1], values[(i * 8) + 5]);
-			triPoints[(i * 12) + 10] = interpolate(isoValue, pointsVec[(i * 8) + 2], pointsVec[(i * 8) + 6], values[(i * 8) + 2], values[(i * 8) + 6]);
-			triPoints[(i * 12) + 11] = interpolate(isoValue, pointsVec[(i * 8) + 3], pointsVec[(i * 8) + 7], values[(i * 8) + 3], values[(i * 8) + 7]);
+			if(edgeTable[index] &    1)
+				triPoints[(i * 12) +  0] = interpolate(isoValue, pointsVec[(i * 8) + 0], pointsVec[(i * 8) + 1], values[(i * 8) + 0], values[(i * 8) + 1]);
+			if(edgeTable[index] &    2)
+				triPoints[(i * 12) +  1] = interpolate(isoValue, pointsVec[(i * 8) + 1], pointsVec[(i * 8) + 2], values[(i * 8) + 1], values[(i * 8) + 2]);
+			if(edgeTable[index] &    4)
+				triPoints[(i * 12) +  2] = interpolate(isoValue, pointsVec[(i * 8) + 2], pointsVec[(i * 8) + 3], values[(i * 8) + 2], values[(i * 8) + 3]);
+			if(edgeTable[index] &    8)
+				triPoints[(i * 12) +  3] = interpolate(isoValue, pointsVec[(i * 8) + 3], pointsVec[(i * 8) + 0], values[(i * 8) + 3], values[(i * 8) + 0]);
+			if(edgeTable[index] &   16)
+				triPoints[(i * 12) +  4] = interpolate(isoValue, pointsVec[(i * 8) + 4], pointsVec[(i * 8) + 5], values[(i * 8) + 4], values[(i * 8) + 5]);
+			if(edgeTable[index] &   32)
+				triPoints[(i * 12) +  5] = interpolate(isoValue, pointsVec[(i * 8) + 5], pointsVec[(i * 8) + 6], values[(i * 8) + 5], values[(i * 8) + 6]);
+			if(edgeTable[index] &   64)
+				triPoints[(i * 12) +  6] = interpolate(isoValue, pointsVec[(i * 8) + 6], pointsVec[(i * 8) + 7], values[(i * 8) + 6], values[(i * 8) + 7]);
+			if(edgeTable[index] &  128)
+				triPoints[(i * 12) +  7] = interpolate(isoValue, pointsVec[(i * 8) + 7], pointsVec[(i * 8) + 4], values[(i * 8) + 7], values[(i * 8) + 4]);
+			if(edgeTable[index] &  265)
+				triPoints[(i * 12) +  8] = interpolate(isoValue, pointsVec[(i * 8) + 0], pointsVec[(i * 8) + 4], values[(i * 8) + 0], values[(i * 8) + 4]);
+			if(edgeTable[index] &  512)
+				triPoints[(i * 12) +  9] = interpolate(isoValue, pointsVec[(i * 8) + 1], pointsVec[(i * 8) + 5], values[(i * 8) + 1], values[(i * 8) + 5]);
+			if(edgeTable[index] & 1024)
+				triPoints[(i * 12) + 10] = interpolate(isoValue, pointsVec[(i * 8) + 2], pointsVec[(i * 8) + 6], values[(i * 8) + 2], values[(i * 8) + 6]);
+			if(edgeTable[index] & 2049)
+				triPoints[(i * 12) + 11] = interpolate(isoValue, pointsVec[(i * 8) + 3], pointsVec[(i * 8) + 7], values[(i * 8) + 3], values[(i * 8) + 7]);
 		}
 	);
 
